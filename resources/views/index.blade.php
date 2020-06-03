3a6ec17920940bd8ae93cd18b2fda83d240fd4ad
jelle <25534@ma-web.nl>
2020-06-03 20:55:10 +0200

    header
    Made landing header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,19 @@
 
 @section('main')
 
-    <h1>test</h1>
+    <div class="head">
+        <div class="img">
+            <div class="overlay"></div>
+        </div>
+        <div class="text">
+            <h1>Jelle<br>Stekelenburg</h1>
+            <p>Front-End Developer</p>
+        </div>
+
+        <div class="text2">
+            <h1>Jelle<br>Stekelenburg</h1>
+            <p>Front-End Developer</p>
+        </div>
+    </div>
 
 @endsection
