3cbcfec1d8bf2d2f8f527c3768eb66a38bcbe3de
jelle <25534@ma-web.nl>
2020-06-07 22:13:46 +0200

    Navigation update
    Added Javascript to nav with scroll detection

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,6 +5,19 @@
 
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/keyframes.css">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+    <script>
+        $(window).on('scroll', function () {
+            if($(window).scrollTop()) {
+                $('nav').addClass('bg');
+            }
+            else {
+                $('nav').removeClass('bg');
+            }
+        })
+
+    </script>
 </head>
 
 <body>
